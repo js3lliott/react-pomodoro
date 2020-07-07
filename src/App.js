7e46d49d426e26938a28037ffdff75d815e6b4c9
Jordan Samek <jordansamek@Jordans-MacBook-Pro.local>
2020-07-07 15:10:47 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
-function App() {
+function padTime(time) {
+  return time.toString().padStart(2, "0");
+}
+
+export default function App() {
+
+  const [title, setTitle] = useState("Let the countdown begin!!!");
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  function startTimer() {
+    setTitle(`You're doing great!`);
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTimeLeft(timeLeft => {
+        if (timeLeft >= 1) return timeLeft - 1;
+
+        //reset timer
+        resetTimer();
+        return 0;
+      });
+    }, 1000);
+  }
+
+  function stopTimer() {
+    clearInterval(intervalRef.current);
+    setTitle("Keep it up!");
+    setIsRunning(false);
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+
+    setTitle("Ready to go again?");
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{title}</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </div>
   );
 }
 
-export default App;
