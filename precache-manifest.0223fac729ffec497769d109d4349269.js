e54d16943fe330ddee3b615db875bea9531b1294
Jordan Samek <jordansamek@Jordans-MacBook-Pro.local>
2020-07-07 15:35:29 -0400

    Updates

---
+++ precache-manifest.0223fac729ffec497769d109d4349269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33be820d5d9cff0a4a6d2f55d90e77a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53844196a76fb7cb5ad0",
+    "url": "/static/css/main.c08a6c2a.chunk.css"
+  },
+  {
+    "revision": "ab877c0bdb569f502885",
+    "url": "/static/js/2.2c030397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c030397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53844196a76fb7cb5ad0",
+    "url": "/static/js/main.d01302d5.chunk.js"
+  },
+  {
+    "revision": "62fa9f18f1e284e170dc",
+    "url": "/static/js/runtime-main.c2686850.js"
+  }
+]);
